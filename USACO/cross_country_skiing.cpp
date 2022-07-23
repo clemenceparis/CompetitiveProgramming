1285cea6ebb75a962d73240c1ad7a1df81b76052
Clemence <clemence.pham@gmail.com>
2022-07-23 16:57:02 +0100

    no message

--- USACO/cross_country_skiing.cpp
+++ USACO/cross_country_skiing.cpp
@@ -6,31 +6,31 @@
 using namespace std;
 
 vector<vector<int>> grid; vector<pair<int, int>> waystation;
-bool visited[500][500] = {false};
+bool visited[500][500] = {false}; int n, m;
 
-void DFS(int r, int c, int l) {
-    if(visited[r][c]) return;
+void DFS(int r, int c, int p, int l) {
+    if(r < 0 || c < 0 || r == n || c == m || abs(p-grid[r][c]) > l || visited[r][c]) return;
     visited[r][c] = true;
-    if(r > 0 && abs(grid[r-1][c]-grid[r][c]) <= l) DFS(r-1, c, l);
-    if(r < grid.size()-1 && abs(grid[r+1][c]-grid[r][c]) <= l) DFS(r+1, c, l);
-    if(c > 0 && abs(grid[r][c-1]-grid[r][c]) <= l) DFS(r, c-1, l);
-    if(c < grid[0].size()-1 && abs(grid[r][c+1]-grid[r][c]) <= l) DFS(r, c+1, l);
+    DFS(r-1, c, grid[r][c], l);
+    DFS(r+1, c, grid[r][c], l);
+    DFS(r, c-1, grid[r][c], l);
+    DFS(r, c+1, grid[r][c], l);
 }
 
-bool check(int r, int c, int l) {
-    for(int i=0; i<grid.size(); i++) for(int j=0; j<grid[0].size(); j++) visited[i][j] = false;
-    DFS(r, c, l);
+bool check(int l) {
+    for(int i=0; i<n; i++) for(int j=0; j<m; j++) visited[i][j] = false;
+    DFS(waystation[0].first, waystation[0].second, grid[waystation[0].first][waystation[0].second], l);
     for(int i=0; i<waystation.size(); i++) {
         if(!visited[waystation[i].first][waystation[i].second]) return false;
     }
     return true;
 }
 
-int firstTrue(int lo, int hi, int r, int c) {
+int firstTrue(int lo, int hi) {
     hi++;
     while (lo < hi) {
         int mid = lo + (hi - lo) / 2;
-        if (check(r, c, mid)) hi = mid;
+        if (check(mid)) hi = mid;
         else lo = mid + 1;
     }
     return lo;
@@ -39,17 +39,16 @@ int firstTrue(int lo, int hi, int r, int c) {
 
 int main() {
     ifstream ifs("ccski.in"); ofstream ofs("ccski.out");
-    int rows, cols; ifs >> rows >> cols;
-    grid = vector<vector<int>>(rows, vector<int>(cols));
+    ifs >> n >> m;
+    grid = vector<vector<int>>(n, vector<int>(m));
 
-    for(int i=0; i<rows; i++) for(int j=0; j<cols; j++) ifs >> grid[i][j];
-    for(int i=0; i<rows; i++) for(int j=0; j<cols; j++) {
+    for(int i=0; i<n; i++) for(int j=0; j<m; j++) ifs >> grid[i][j];
+    for(int i=0; i<n; i++) for(int j=0; j<m; j++) {
         int a; ifs >> a;
         if(a == 1) waystation.push_back({i, j});
     }
 
-    ofs << firstTrue(0, 1e9, waystation[0].first, waystation[0].second); return 0;
-            
+    ofs << firstTrue(0, 1e9);
     return 0;
 }
 
